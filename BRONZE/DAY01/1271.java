cea43e559fbe4b2ba7e330d561c3b33bc4ebd402
이소연 <122617471+silviasylee03@users.noreply.github.com>
2024-01-15 14:44:14 +0900

    Create 1271.java

---
+++ BRONZE/DAY01/1271.java
@@ -0,0 +1,17 @@
+// 1271
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+  public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger n = sc.nextBigInteger();
+		BigInteger m = sc.nextBigInteger();
+		
+		sc.close();
+		
+		System.out.println(n.divide(m));
+		System.out.println(n.remainder(m));
+	}
+}
